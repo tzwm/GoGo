e49fc1b4e9b2c39ff365dfe60ae23f6391e66c49
tzwm <wangjunzhe4444@gmail.com>
2013-05-21 11:13:38 +0800

    add Goban

--- guimainwindow.c
+++ guimainwindow.c
@@ -2,6 +2,42 @@
 
 GtkWidget* mainWindow;
 GtkWidget* menubar;
+GtkWidget* drawingareaGoban;
+
+gint arearedrawGoban(GtkWidget* widget, GdkEventExpose* event, gpointer data){
+    int i, j;
+    GdkColor color;
+    GdkDrawable* canvas;
+    GdkGC* gc;
+
+    canvas = widget->window;
+    gc = widget->style->fg_gc[GTK_WIDGET_STATE(widget)];
+    
+    color.red = 65535;
+    color.green = 47545;
+    color.blue = 2855;
+    gdk_gc_set_rgb_fg_color(gc, &color);
+    for(i = 0; i<GOBAN_WIDTH; i++)
+       gdk_draw_line(canvas, gc, 0, i, GOBAN_HEIGHT, i);
+
+
+    color.red =25723;
+    color.green = 26985;
+    color.blue = 5140;
+    gdk_gc_set_rgb_fg_color(gc, &color);
+    for(i = 0; i<= GOBAN_WIDTH; i+=SIZE)
+        gdk_draw_line(canvas, gc, i, 0, i, GOBAN_HEIGHT);
+    for(i = 0; i<= GOBAN_HEIGHT; i+=SIZE)
+        gdk_draw_line(canvas, gc, 0, i, GOBAN_WIDTH, i);
+
+    color.red = 0;
+    color.green = 0;
+    color.blue = 0;
+    gdk_gc_set_rgb_fg_color(gc, &color);
+
+    return TRUE;
+}
+
 
 void init_mainWindow(){
     mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -30,6 +66,23 @@ void init_mainWindow(){
 
     g_signal_connect(G_OBJECT(quit), "activate", 
                         G_CALLBACK(gtk_main_quit), NULL);
+
+
+    GtkWidget* hbox = gtk_hbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(vbox), hbox);
+    
+    drawingareaGoban = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawingareaGoban, GOBAN_WIDTH, GOBAN_HEIGHT);
+
+    g_signal_connect(G_OBJECT(drawingareaGoban), "expose_event",
+                    G_CALLBACK(arearedrawGoban), NULL);
+
+    GtkWidget* fixed = gtk_fixed_new();
+    gtk_widget_set_usize(fixed, 20, 20);
+    gtk_fixed_put(GTK_FIXED(fixed), drawingareaGoban, 0, 0);
+    gtk_container_add(GTK_CONTAINER(hbox), fixed);
+    
+    
 }
 
 int main(int argc, char* argv[]){
--- guimainwindow.h
+++ guimainwindow.h
@@ -1,8 +1,14 @@
 #include<gtk/gtk.h>
 #include<string.h>
 
-#define WINDOW_WIDTH    1000
+#define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   600
+#define SIZE            30
+#define NUM             19
+#define GOBAN_WIDTH     (SIZE*(NUM-1)+1)
+#define GOBAN_HEIGHT    (SIZE*(NUM-1)+1)
+
+
 
 extern GtkWidget* mainWindow;
 
